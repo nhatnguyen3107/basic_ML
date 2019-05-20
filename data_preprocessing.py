6705ff8d1e25e70445c4b40112f80d2fa9ac9a1d
nhatnguyen3107 <nhatnguyen3107@gmail.com>
2019-05-20 17:59:12 +0700

    fix error concat function

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -14,7 +14,6 @@ def load_data(train_file, test_file): #load data from file name
 def clean_data(data_frame): # clean null and duplicated points
     # REMOVE NULL DATA POINTS
     null_counter = data_frame.isnull().sum(axis=0)
-    # print(test_data_frame.isnull().sum(axis = 0))
     for column, count_null in null_counter.items():
         if count_null > 0.3*data_frame.shape[0]:
             data_frame.drop(column, axis=1, inplace = True)
@@ -25,8 +24,8 @@ def clean_data(data_frame): # clean null and duplicated points
     
 def feature_engineering(data_frame):
     # Date_of_Journey Column processing, pick month and day_of_week (has a little range)
-    data_frame['Month_of_Journey'] = pd.to_datetime(data_frame['Date_of_Journey'], format='%d/%m/%Y').dt.month.astype(int)
-    data_frame['Day_of_Week'] = pd.to_datetime(data_frame['Date_of_Journey'], format='%d/%m/%Y').dt.dayofweek.astype(int)
+    data_frame['Month_of_Journey'] = pd.to_datetime(data_frame['Date_of_Journey'], format='%d/%m/%Y').dt.month.astype(float)
+    data_frame['Day_of_Week'] = pd.to_datetime(data_frame['Date_of_Journey'], format='%d/%m/%Y').dt.dayofweek.astype(float)
     data_frame.drop('Date_of_Journey', axis=1, inplace=True)
 
     # Source and Destination processing, synchronize 2 near place or be just the same place
@@ -52,12 +51,12 @@ def feature_engineering(data_frame):
         elif len(spl) == 1 and spl[0][-1] == 'm':
             duration_train[i] = '0h ' + duration_train[i]
     data_frame['Duration'] = [int(duration_train[i].split()[0][:-1])*60 + int(duration_train[i].split()[1][:-1]) for i in range(len(duration_train))]
-    data_frame['Duration'] = data_frame['Duration'].astype(int)
+    data_frame['Duration'] = data_frame['Duration'].astype(float)
     
     #Total_Stop Column processing
     # print(data_frame['Total_Stops'].value_counts())
     data_frame.replace({'Total_Stops':{'non-stop':0, '1 stop':1, '2 stops': 2, '3 stops': 3, '4 stops': 4}}, inplace=True)
-    data_frame['Total_Stops'] = data_frame['Total_Stops'].astype(int)
+    data_frame['Total_Stops'] = data_frame['Total_Stops'].astype(float)
     
     #Additional_Info Column processing
     # print(data_frame['Additional_Info'].value_counts())
@@ -68,23 +67,19 @@ def feature_engineering(data_frame):
     return data_frame
 
 def feat_decoding_and_scaling(X_feats_set):
-    # print(X_feats_set.head())
-    X_categorical_feats = X_feats_set.select_dtypes(exclude=['int'])
-    X_numerical_feats = X_feats_set.select_dtypes(include=['int'])
-    # print(X_categorical_feats.shape)
-    # print(X_numerical_feats.shape)
+    X_categorical_feats = X_feats_set.select_dtypes(exclude=['int', 'float'])
+    X_numerical_feats = X_feats_set.select_dtypes(include=['int', 'float'])
     # ENCODING CATEGORICAL FEATURES
     label_encoder = LabelEncoder()
     X_categorical_feats = X_categorical_feats.apply(label_encoder.fit_transform)
-    print('cate',X_categorical_feats.shape)
 
     # SCALING NUMERICAL FEATURES with Standard Scaler
     standard_scaler = StandardScaler()
     X_scaled = standard_scaler.fit_transform(X_numerical_feats)
     X_numerical_feats = pd.DataFrame(data=X_scaled, columns=X_numerical_feats.columns)
-    print('num',X_numerical_feats.shape)
+    X_categorical_feats.reset_index(drop=True, inplace=True)
+    X_numerical_feats.reset_index(drop=True, inplace=True)
     X_feats_set_after = pd.concat([X_categorical_feats, X_numerical_feats], axis = 1)
-    # print('after',X_feats_set_after.shape)
     return X_feats_set_after
 
 def select_features_by_random_forest_regressor(X, y):
@@ -111,20 +106,19 @@ if __name__ == "__main__":
     
     train_feat_frame = feature_engineering(clean_train_data_frame.copy())
     test_feat_frame =  feature_engineering(clean_test_data_frame.copy())
-    train_feat_frame.to_excel('raw_feature/train_data_feature.xlsx')
-    test_feat_frame.to_excel('raw_feature/test_data_feature.xlsx')
+    train_feat_frame.to_excel('raw_feature/train_data_feature.xlsx', index=False)
+    test_feat_frame.to_excel('raw_feature/test_data_feature.xlsx',index=False)
     print('\nAfter engineering data')
     print('The Number of features: ',train_feat_frame.shape[1])
     print('Include: ', train_feat_frame.columns.tolist())
     
     X_feats_train = train_feat_frame.drop('Price', axis=1)
     y_target_train = np.log1p(train_feat_frame['Price'])
-    print(X_feats_train.shape, len(y_target_train))
     final_train_data = feat_decoding_and_scaling(X_feats_train)
     final_train_data['Price'] = y_target_train
     final_test_data = feat_decoding_and_scaling(test_feat_frame)
-    final_train_data.to_excel('final_feature/train_feat_after_tranforming.xlsx')
-    final_test_data.to_excel('final_feature/predict_feat_after_tranforning.xlsx')
+    final_train_data.to_excel('final_feature/train_feat_after_tranforming.xlsx',index=False)
+    final_test_data.to_excel('final_feature/predict_feat_after_tranforning.xlsx',index=False)
     
     select_features_by_random_forest_regressor(final_train_data.drop('Price', axis=1),y_target_train)
     print('Preprocessing data is complete!')
